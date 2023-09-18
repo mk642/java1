9da8cd191cf514a62fae4c205e7f6ba58caa6567
mk642 <53805997+mk642@users.noreply.github.com>
2023-09-18 21:27:47 +0900

    Add files via upload

---
+++ Exercise.java
@@ -0,0 +1,29 @@
+package java1;
+
+public class Exercise {
+	public static void main(String[]args) {
+		//1번  =class 
+		/*2번 
+		 (1)public static void main(String[]args){}를 뺴먹었다.
+		 (2)SampleProgram 
+		 (3)javac SampleProgram.java
+		 (4)java SampleProgram    */
+		//3번 int _i; , int %j; , double 1var;
+		/*4번 
+		 (1)int height;
+		 (2)double size = 0.25; 
+		 (3)total = height + size;
+		 (4)char c = 'a';
+		 (5)Stirng name = "김민기";*/
+		/*5번
+		 (1)67 +12.8 = 79.8 ,double 
+		 (2)10/3 = 3 int 
+		 (3)10.0/3 = 3.333333333333333.5 , double
+		 (4) 10==9 = false , boolean
+		 * */
+		
+	
+		
+	}
+
+}
